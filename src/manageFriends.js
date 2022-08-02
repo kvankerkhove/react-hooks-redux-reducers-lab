d7486bc91f24812c5fdc57dff2a392b47f5f6376
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-08-02 19:43:12 -0400

    commit tree 51b0b3bc7771f1d323f34fc542d0902ca8f118aa

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,12 @@
 export function manageFriends(state, action) {
   // your code here
+  switch(action.type){
+    case "friends/add":
+      return {friends: [...state.friends, action.payload]};
+    case "friends/remove":
+      let newFriendsArray = state.friends.filter(friend => friend.id !== action.payload)
+      return {friends: newFriendsArray}
+    default:
+      return state
+  }
 }
